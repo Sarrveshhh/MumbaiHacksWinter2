71d8a292b8079e917cae068aac977dc327334579
Sarrveshhh <sarrvesharyaa@gmail.com>
2023-12-17 00:13:41 +0530

    "Added the typing effect to the AI Response."

--- client/src/Components/AIBubble.jsx
+++ client/src/Components/AIBubble.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useEffect, useState}from "react";
 import "../App.css";
 import { TfiGithub } from "react-icons/tfi";
-
 function AIBubble({ text }) {
+  const [typeText,setTypeText] = useState("");
+  useEffect(() => {
+    const textToType = async () => {
+      for (let index = 0; index < text.length; index++) {
+        setTypeText(text.slice(0, index));
+        await new Promise(resolve => setTimeout(resolve, 10));
+      }
+    };
+  
+    textToType();
+  }, []);
   return (
     <div className="bubble-ai">
       <div className="bubble-logo">
         <TfiGithub /> <p>GitHubGPT</p>
       </div>
-      <div className="bubble-text">{text}</div>
+      <div className="bubble-text">{typeText}</div>
     </div>
   );
 }
