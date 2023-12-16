93d53e9c217fcb9d1a2ff97e481d1ac2d8475064
Kishan Kokal <kokalkishan.official@gmail.com>
2023-12-16 14:11:25 +0530

    Refactor App.js component

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,23 @@
-<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
 import ChatApp from "./Components/ChatApp.js";
 import Chat from "./Components/Chat";
-=======
 import './App.css';
 import ChatApp from './Components/ChatApp.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home.jsx';
->>>>>>> 1d4523d9740c8bf5cb277cee74888db2f239e606
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       {/* <ChatApp /> */}
       <Chat />
-=======
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/chat" element={<ChatApp />} />
         </Routes>
       </BrowserRouter>
->>>>>>> 1d4523d9740c8bf5cb277cee74888db2f239e606
     </div>
   );
 }
