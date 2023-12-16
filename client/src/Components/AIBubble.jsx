4a19f1f08d984173f9fa75d09b64d1ea98ebff61
Kishan Kokal <kokalkishan.official@gmail.com>
2023-12-16 20:33:19 +0530

    Add response loader and update AIBubble text

--- client/src/Components/AIBubble.jsx
+++ client/src/Components/AIBubble.jsx
@@ -2,20 +2,13 @@ import React from "react";
 import "../App.css";
 import { TfiGithub } from "react-icons/tfi";
 
-function AIBubble() {
+function AIBubble({ text }) {
   return (
     <div className="bubble-ai">
       <div className="bubble-logo">
         <TfiGithub /> <p>GitHubGPT</p>
       </div>
-      <div className="bubble-text">
-        Yes, there have been ongoing trade tensions between China and the United
-        States of America. While I don't have specific information about the
-        involvement of Saudi Arabia or other countries as proxies in this trade
-        war, it is not uncommon for countries to seek alliances or partnerships
-        to gain leverage in trade disputes. Would you like me to look up more
-        information on this topic?
-      </div>
+      <div className="bubble-text">{text}</div>
     </div>
   );
 }
--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AiOutlineSend } from "react-icons/ai";
 import { TfiGithub } from "react-icons/tfi";
 import "../App.css";
 import { useState } from "react";
 import AIBubble from "./AIBubble";
 import HumanBubble from "./HumanBubble";
+import axios from "axios";
+import HashLoader from "react-spinners/HashLoader";
 
 function Chat() {
   const [chats, setChats] = useState([]);
   const [text, setText] = useState("");
-  const handleSubmit = () => {
-    chats.push({ role: "user", text: text });
+  const [responseLoading, setResponseLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    setResponseLoading(true);
+    setChats([...chats, { role: "user", text: text }]);
+    let payload = { question: text };
+    let response = await axios.post("http://localhost:8000/query/", payload);
+    setChats((chats) => [
+      ...chats,
+      { role: "bot", text: response.data.message.content },
+    ]);
+    setResponseLoading(false);
     setText("");
   };
   return (
@@ -22,11 +34,23 @@ function Chat() {
 
       <div className="outer-container">
         <div className="output-window">
-          {chats.map((ele) => (
-            <HumanBubble text={ele.text} />
-          ))}
+          {chats.map((ele) =>
+            ele.role === "user" ? (
+              <HumanBubble key={ele.id} text={ele.text} />
+            ) : (
+              <AIBubble key={ele.id} text={ele.text} />
+            )
+          )}
         </div>
         <div className="input-window">
+          <HashLoader
+            className="response-loader"
+            color={"white"}
+            loading={responseLoading}
+            size={40}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
           <input
             type="text"
             onChange={(e) => {
