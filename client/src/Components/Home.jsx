163e3f395aa29c6aa67757b084bb6d7786b38ed6
Kishan Kokal <kokalkishan.official@gmail.com>
2023-12-16 17:43:12 +0530

    Phat hua sila

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -47,31 +47,29 @@ export default function Home({ setIsLoading }) {
         <p className="text-sm text-white mb-8">
           Just paste your GitHub repository link.
         </p>
-        <div className="w-1/3  shadow-lg rounded-lg">
+        <div className="w-1/3  shadow-lg rounded-lg flex flex-col items-center">
           <input
             type="text"
             value={link}
             onChange={handleLinkChange}
             placeholder="https://github.com/<username>/<repo-name>"
-            className=" p-4 w-full bg-white rounded-lg focus:outline-none text-black"
-          />
-        </div>
-        <button className="w-1/5 py-3 mt-10 bg-white text-black uppercase tracking-wider font-semibold rounded-lg hover:opacity-75 duration-200 mt-1">
+            className=" p-4 w-full bg-white rounded-lg focus:outline-none text-black mb-4"
           />
           <input
             type="text"
             value={branch}
             onChange={handleBranchChange}
             placeholder="Enter branch"
-            className=" p-4 text-black w-full bg-white rounded-lg focus:outline-none"
+            className=" p-4 w-full bg-white rounded-lg focus:outline-none text-black"
           />
+          <button
+            onClick={handleSubmit}
+            className="w-200 py-3 mt-10 bg-white text-black uppercase tracking-wider font-semibold rounded-lg hover:opacity-75 duration-200 mt-1 p-4"
+          >
+            Start Analyzing
+          </button>
         </div>
-        <button
-          onClick={handleSubmit}
-          className="w-1/5 py-3 mt-10 bg-black text-white uppercase tracking-wider font-semibold rounded-lg hover:bg-gray-800 transition-colors duration-200 "
-        >
-          Start Analyzing
-        </button>
+        {/* <button className="w-1/5 py-3 mt-10 bg-white text-black uppercase tracking-wider font-semibold rounded-lg hover:opacity-75 duration-200 mt-1"></button> */}
       </div>
     </>
   );
